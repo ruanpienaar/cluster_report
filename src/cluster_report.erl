b85db20880a457504368ba9f4a9b741c1ef25f10
ruanpienaar <ruan800@gmail.com>
2017-03-02 12:53:44 +0000

    Check module consistency across a list of nodes@

--- src/cluster_report.erl
+++ src/cluster_report.erl
@@ -5,9 +5,13 @@
     remove_node/1,
     cluster_nodes/0,
     cluster_modules/0, cluster_modules/1,
+    cluster_modules_extra_info/0, cluster_modules_extra_info/1,
     cluster_apps/0
 ]).
 %% application:get_key(APP, modules).
+
+%%------------------------------------------------------------------------------
+%% API
 
 start() ->
     ok = application:start(hawk),
@@ -32,46 +36,73 @@ cluster_nodes() ->
 
 cluster_modules() ->
     try
-        do_cluster_modules()
+        cluster_module_consistency(do_cluster_modules())
     catch
         C:E ->
-            {error, C, E}
+            {error, C, E, erlang:get_stacktrace()}
     end.
 
 cluster_modules(App) ->
     try
-        do_cluster_modules(App)
+        cluster_module_consistency(do_cluster_modules(App))
     catch
         C:E ->
-            {error, C, E}
+            {error, C, E, erlang:get_stacktrace()}
     end.
 
+%% XXX: include module info here...
+%% Maybe use the crell remove injection stuff
+cluster_modules_extra_info() ->
+    ok.
+
+cluster_modules_extra_info(App) ->
+    ok.
+
+%%------------------------------------------------------------------------------
+
 do_cluster_modules() ->
-    NodesApps = lists:map(fun(Node) ->
+    lists:map(fun(Node) ->
         case rpc:call(Node, code, all_loaded, []) of
             Res when is_list(Res) ->
-                {Node, Res};
+                {Node, lists:sort(Res)};
             {badrpc,R} ->
                 throw({badrpc,R})
         end
     end, cluster_nodes()).
 
 do_cluster_modules(App) ->
-    NodesApps = lists:map(fun(Node) ->
+    lists:map(fun(Node) ->
         case rpc:call(Node, application, get_key, [App, modules]) of
             {ok,Res} when is_list(Res) ->
-                {Node, Res};
+                {Node, lists:sort(Res)};
             {badrpc,R} ->
                 throw({badrpc,R})
         end
     end, cluster_nodes()).
 
 cluster_apps() ->
-    NodesApps = lists:map(fun(Node) ->
+    lists:map(fun(Node) ->
         case rpc:call(Node, application, which_applications, []) of
             Res when is_list(Res) ->
-                {Node, Res};
+                {Node, lists:sort(Res)};
             {badrpc,R} ->
                 throw({badrpc,R})
         end
     end, cluster_nodes()).
+
+cluster_module_consistency(L) ->
+    cluster_module_consistency(L,[]).
+
+cluster_module_consistency([],R) ->
+    R;
+cluster_module_consistency([{Node,Mods}|T],[]) ->
+    cluster_module_consistency(T,[{[Node],Mods}]);
+cluster_module_consistency([{Node,Mods}|T],R) ->
+    cluster_module_consistency(T, compare_others({Node,Mods}, R, R)).
+
+compare_others({Node, Mods}, R, []) ->
+    [{Node, Mods}|R];
+compare_others({Node, Mods}, R, [{Nodes,Mods}|T]) ->
+    lists:keyreplace(Nodes, 1, R, {[Node|Nodes], Mods});
+compare_others({Node, Mods}, R, [{_,_}|T]) ->
+    compare_others({Node, Mods}, R, T).
